ea1090aa643e42ab5517ac01adce183daf70d174
Saieebawake <saieebawake273@gmail.com>
2026-08-26 15:06:42 +0530

    Buid Tic Tac Toe Game

---
+++ .history/script_20260826130736.js
@@ -0,0 +1,12 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn"); 
+
+let turnO = true;
+
+const winPatterns = [
+    [0 ,1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    []
+]
